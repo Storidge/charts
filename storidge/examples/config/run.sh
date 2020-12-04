bb2034bf3207edfce50c2b81c6b9256938197599
Jonathan Kong <jkong@storidge.com>
2020-12-03 18:53:56 -0800

    Remove kubectl binaries

--- storidge/examples/config/run.sh
+++ storidge/examples/config/run.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-#set -euxo pipefail
-set -eux
-
 # move storidge-config file
 cat /secret/storidge-config > /storidge/storidge-config
-# identify config-move pod on local node and delete
-kubectl get po
-
-# log completion status
-echo "done"
-
-# use TERM signal to exit daemonset
-trap 'exit' TERM
 
 # stop container from exiting and k8s restarting daemonset
 while true; do sleep 1; done
+
